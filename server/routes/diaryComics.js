6aeaa43058b8bb7a4e0b75f8e896613b2747ad06
Vanessa Gurung <vanessagur@gmail.com>
2022-10-20 20:27:38 +1300

    get req for diary comics made

---
+++ server/routes/diaryComics.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const db = require('../db/diaryComics')
+
+const router = express.Router()
+
+module.exports = router
+
+// GET /api/v1/diaryComics
+router.get('/', async (req, res) => {
+  try {
+    const diaryComic = await db.getDiaryComic()
+    res.json({ diaryComic })
+  } catch (err) {
+    console.error(err)
+    res.status(500).send(err.message)
+  }
+})
