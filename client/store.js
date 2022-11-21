fa42fcff26ab12938c0d94dd277d9dbf90d1ad16
vanessa-gurung <107532766+vanessa-gurung@users.noreply.github.com>
2022-11-21 14:36:53 +1300

    Update store.js

--- client/store.js
+++ client/store.js
@@ -1,10 +1,1 @@
-import { configureStore } from '@reduxjs/toolkit'
-import fruits from './slices/fruits'
 
-const store = configureStore({
-  reducer: {
-    fruits,
-  },
-})
-
-export default store
