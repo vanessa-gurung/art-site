3e2d5f4bf5090c30ea0a5ca1854ba4ae38db9971
vanessa-gurung <107532766+vanessa-gurung@users.noreply.github.com>
2022-11-21 14:38:26 +1300

    Delete diarycomics.js

--- server/db/diarycomics.js
+++
@@ -1,8 +0,0 @@
-const connection = require('./connection')
-
-module.exports = { getDiaryComic }
-
-// For diary comic images
-function getDiaryComic(db = connection) {
-  return db('diaryComics').select('id', 'date', 'diary_comic')
-}
