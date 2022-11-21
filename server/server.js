c1a144ad9520fc12db95d7f4ae700fc994955eb4
vanessa-gurung <107532766+vanessa-gurung@users.noreply.github.com>
2022-11-21 14:41:52 +1300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,1 @@
-const express = require('express')
-const path = require('path')
 
-const diaryComicRoutes = require('./routes/diaryComics')
-
-const server = express()
-
-server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
-
-server.use('/api/v1/diaryComics', diaryComicRoutes)
-
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'))
-})
-
-module.exports = server
