01e43c197fa8bbb9871def9989a53d3f89074a6c
Vanessa Gurung <vanessagur@gmail.com>
2022-11-29 15:50:24 +1300

    diary comic function created

---
+++ server/db/diaryComics.js
@@ -0,0 +1,15 @@
+const connection = require('./connection')
+
+module.exports = {
+  showDiaryComics
+}
+
+// Diary comic component 
+
+function showDiaryComics(db = connection) {
+  return db('diary_comics').select(
+    'id',
+    'date',
+    'diary_comic',
+  )
+}
